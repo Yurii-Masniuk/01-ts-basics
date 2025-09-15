33183f29df729fc25300fce5d675da3dd6b9fa4c
Yurii <yerikstar@gmail.com>
2025-09-16 00:00:18 +0300

    task-2

---
+++ src/task-1.ts
@@ -0,0 +1,7 @@
+const name: string = 'Alice';
+let age: number = 30;
+const isOnLine: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnLine}`);
