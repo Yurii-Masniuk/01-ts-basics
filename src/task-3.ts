b6e4d5e1428eed2d6df3d32d78042f0413bed4db
Yurii <yerikstar@gmail.com>
2025-09-16 19:49:25 +0300

    task-3

---
+++ src/task-3.ts
@@ -0,0 +1,15 @@
+interface Product {
+    id: number,
+    title: string,
+};
+
+const usernames: string[] = ['alice', 'bob', 'charlie'];
+const ratings: number[] = [4.5, 3.8, 5];
+const products: Product[] = [
+    { id: 1, title: 'phone' },
+    { id: 2, title: 'laptop' },
+];
+
+console.log(`Usernames: ${JSON.stringify(usernames)}`);
+console.log(`Ratings: ${JSON.stringify(ratings)}`);
+console.log(`Products: ${JSON.stringify(products)}`);
